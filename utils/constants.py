6100a5418aa7070c308058bea6b8ce0c95095877
Felipe Melchior <fehmel@gmail.com>
2020-11-10 22:44:45 -0300

    chore: create vagrantfiles and enhance payload list

--- utils/constants.py
+++ utils/constants.py
@@ -1,6 +1,6 @@
 # Project constants
 
 payloads_dir = 'payloads'
-accepted_payloads_choice = ['load1.txt', 'load2.txt', 'all']
+accepted_payloads_choice = ['load1.txt', 'load2.txt', 'load3.txt', 'all']
 host_external_list = ['https://google.com', 'https://facebook.com', 'https://cloudflare.com'] 
 
